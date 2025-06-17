e9914f89b6aa9ec12b50702b37df5e1e789d3856
TG-Badshah <king1294anand@gmail.com>
2025-06-17 12:47:17 +0530

    Update play.py

--- GayuMusic/utils/inline/play.py
+++ GayuMusic/utils/inline/play.py
@@ -5,7 +5,6 @@ import config
 from GayuMusic import app
 from GayuMusic.utils.formatters import time_to_seconds
 
-
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
         [
@@ -27,69 +26,116 @@ def track_markup(_, videoid, user_id, channel, fplay):
     ]
     return buttons
 
-
 def stream_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "𝄞─────────"
-    elif 10 < umm < 20:
-        bar = "━𝄞────────"
-    elif 20 <= umm < 30:
-        bar = "━━𝄞───────"
-    elif 30 <= umm < 40:
-        bar = "━━━𝄞──────"
-    elif 40 <= umm < 50:
-        bar = "━━━━𝄞─────"
-    elif 50 <= umm < 60:
-        bar = "━━━━━𝄞────"
-    elif 60 <= umm < 70:
-        bar = "━━━━━━𝄞───"
-    elif 70 <= umm < 80:
-        bar = "━━━━━━━𝄞──"
-    elif 80 <= umm < 95:
-        bar = "━━━━━━━━𝄞─"
+    Bad = math.floor(percentage)
+    # Progress bar (ba)
+    if 0 < Bad <= 10:
+        ba = "𝄞─────────"
+    elif 10 < Bad < 20:
+        ba = "━𝄞────────"
+    elif 20 <= Bad < 30:
+        ba = "━━𝄞───────"
+    elif 30 <= Bad < 40:
+        ba = "━━━𝄞──────"
+    elif 40 <= Bad < 50:
+        ba = "━━━━𝄞─────"
+    elif 50 <= Bad < 60:
+        ba = "━━━━━𝄞────"
+    elif 60 <= Bad < 70:
+        ba = "━━━━━━𝄞───"
+    elif 70 <= Bad < 80:
+        ba = "━━━━━━━𝄞──"
+    elif 80 <= Bad < 95:
+        ba = "━━━━━━━━𝄞─"
     else:
-        bar = "━━━━━━━━━𝄞"
+        ba = "━━━━━━━━━𝄞"
+
+    # Text bar (bar)
+    if 0 < Bad <= 5:
+        bar = "𝐁𝖾𝗌𝗍 𝐅𝖾α𝗍υ𝗋𝖾𝗌"
+    elif 5 <= Bad < 10:
+        bar = "𝐅α𝗏ⱺ𝗋𝗂𝗍𝖾 ρᥣα𝗒ᥣ𝗂𝗌𝗍"
+    elif 10 <= Bad < 15:
+        bar = "𝐌𝗂ᥣᥣ𝗂ⱺ𐓣 𝐒ⱺ𐓣𝗀𝗌"
+    elif 15 <= Bad < 20:
+        bar = "𝐄α𝗌𝗂ᥣ𝗒 𝐒𝗍𝗋𝖾αꭑ"
+    elif 20 <= Bad < 25:
+        bar = "𝐒𝗍𝗋𝖾αꭑ𝗂𐓣𝗀"
+    elif 25 <= Bad < 30:
+        bar = "𝐁𝗂𝗀 𝐃α𝗍αᑲα𝗌ɦ"
+    elif 30 <= Bad < 35:
+        bar = "𝐃ⱺω𐓣ᥣⱺαᑯ 𝐌υ𝗌𝗂𝖼"
+    elif 35 <= Bad < 40:
+        bar = "𝐅α𝗏ⱺ𝗋𝗂𝗍𝖾 ρᥣα𝗒ᥣ𝗂𝗌𝗍"
+    elif 40 <= Bad < 45:
+        bar = "𝐋α𝗀 𝐅𝗋𝖾𝖾"
+    elif 45 <= Bad < 50:
+        bar = "𝐌υʂιƈ 𝐒𝗍υᑯ𝗂ⱺ"
+    elif 50 <= Bad < 55:
+        bar = "𝐌υʂιƈ 𝐀ρρ"
+    elif 55 <= Bad < 60:
+        bar = "𝐄𐓣𝗃ⱺ𝗒 𝐌υʂιƈ 𝐎𐓣 𝐓𝖾ᥣ𝖾𝗀𝗋αꭑ"
+    elif 60 <= Bad < 65:
+        bar = "𝐄𐓣𝗃ⱺ𝗒 𝐌υ𝗌𝗂𝖼"
+    elif 65 <= Bad < 70:
+        bar = "𝑴𝒖𝒔𝒊𝒄"
+    elif 70 <= Bad < 75:
+        bar = "𝐁𝖾𝗌𝗍 𝐅𝖾α𝗍υ𝗋𝖾𝗌"
+    elif 80 <= Bad < 80:
+        bar = "𝐌𝗂ᥣᥣ𝗂ⱺ𐓣 𝐒ⱺ𐓣𝗀𝗌"
+    elif 80 <= Bad < 85:
+        bar = "𝐄α𝗌𝗂ᥣ𝗒 𝐒𝗍𝗋𝖾αꭑ"
+    elif 85 <= Bad < 90:
+        bar = "𝐋ⱺω-𝐒ρ𝖾𝖾ᑯ 𝐒𝗍𝗋𝖾αꭑ𝗂𐓣𝗀"
+    elif 90 <= Bad < 95:
+        bar = "𝐒ⱺ𐓣𝗀 𝚰𝗌 𝐀ᑲⱺυ𝗍 𝐓ⱺ 𝐄𐓣ᑯ"
+    else:
+        bar = "𝐓ɦ𝖾 𝐒ⱺ𐓣𝗀 𝚰𝗌 𝐎𝗏𝖾𝗋"
+
     buttons = [
-        [
+     [
             InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
+                text=f"{played} {ba} {dur}",
                 callback_data="GetTimer",
             )
         ],
         [
-            #InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(
+                text=f"{bar}",
+                callback_data="GetTimer",
+            )
+        ],
+        [
             #InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            #InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
-            #InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),  
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            #InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
        
-        [InlineKeyboardButton(
-                text="• 𝐔ᴘᴅᴀᴛᴇs •", url=config.SUPPORT_CHANNEL
-            )],
+        [
+             InlineKeyboardButton(text="「 ✦ 𝐂𝐋𝐈𝐂𝐊 ✦ 」", url=f"https://t.me/New_sanatani"),
+          ],
     ]
     return buttons
-
 
 def stream_markup(_, chat_id):
     buttons = [
         [
-            #InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
             #InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            #InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
-            #InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),  
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            #InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-      
-        [InlineKeyboardButton(
-                text="• 𝐔ᴘᴅᴀᴛᴇs •", url=config.SUPPORT_CHANNEL
-            )],
+        [
+             InlineKeyboardButton(text="「 ✦ 𝐂𝐋𝐈𝐂𝐊 ✦ 」", url=f"https://t.me/New_sanatani"),
+          ],
     ]
     return buttons
 
@@ -99,11 +145,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"AlonePlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"BadPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"AlonePlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"BadPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
